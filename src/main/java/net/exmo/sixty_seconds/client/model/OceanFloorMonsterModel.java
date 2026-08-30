ba99916cc71e32597009f3b8fadf6d3f5f5aeadd
haiman <83174104+haiman233@users.noreply.github.com>
2026-08-30 22:49:25 +0800

    1

--- src/main/java/net/exmo/sixty_seconds/client/model/OceanFloorMonsterModel.java
+++ src/main/java/net/exmo/sixty_seconds/client/model/OceanFloorMonsterModel.java
@@ -220,7 +220,7 @@ public class OceanFloorMonsterModel extends EntityModel<OceanFloorMonsterEntity>
         PartDefinition el = root.addOrReplaceChild("eel", CubeListBuilder.create(), PartPose.ZERO);
         PartDefinition eelHead = el.addOrReplaceChild("head",
                 CubeListBuilder.create().texOffs(0, 0).addBox(-3.0F, -3.0F, -8.0F, 6.0F, 6.0F, 8.0F),
-                PartPose.offset(0.0F, 21.0F, -16.0F));
+                PartPose.offset(0.0F, 21.0F, -8.0F));
         eelHead.addOrReplaceChild("jaw",
                 CubeListBuilder.create().texOffs(30, 0).addBox(-3.0F, 0.0F, -7.0F, 6.0F, 2.0F, 7.0F),
                 PartPose.offsetAndRotation(0.0F, 3.0F, -8.0F, 0.18F, 0.0F, 0.0F));
